611ba68aefcb6a50764fb61dec2e5d519de5d605
Khursheed <ka.pixelbits@gmail.com>
2025-11-17 19:25:30 +0500

    adjusted responsiveness

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/Figma-Design-to-HTML-Tailwind-CSS/',
   plugins: [tailwindcss()],
 });
